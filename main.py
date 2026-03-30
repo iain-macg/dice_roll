0da44f398ac4abb1ff81dd4097546e5987b89427
iain-macg <dougiemacg2@gmail.com>
2026-03-30 18:28:05 +0100

    refactor: implement DRY principle for input validation

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 import random
 
+def validate_number(number, range_end):
+    try:
+        num = int(number)
+    except ValueError:
+        print(f"Invalid input, enter a number 1 - {range_end}")
+    else:
+        if 0 <  num <= range_end:
+            return True 
+        else:
+           print(f"Invalid input, enter a number 1 - {range_end}") 
+
 def choose_num_dice():
     num_dice_valid = False
     while not num_dice_valid:
-        try:
-            num = int(input("how many dice would you like to roll? "))
-        except ValueError:
-            print("Invalid input, enter a number 1 - 5")
-        else:
-            if 0 <  num < 6:
-                num_dice_valid = True
-            else:
-                print("Invalid input, enter a number 1 - 5")
-    return num
+        num = input("how many dice would you like to roll? ")
+        num_dice_valid = validate_number(num, 5)
+    return int(num)
 
 def choose_num_sides():
     sides_valid = False
     while not sides_valid:
-        try:
-            sides = int(input("how many sides would you like the dice to have? "))
-        except ValueError:
-            print("Invalid input, enter a number 1 - 21")
-        else:
-            if 0 < sides < 22:
-                sides_valid = True
-            else:
-                print("Invalid input, enter a number 1 - 21")
-    return sides
+        sides = input("how many sides would you like the dice to have? ")
+        sides_valid = validate_number(sides, 20)
+    return int(sides)
 
 def roll_dice(num, sides):
     dice_list = [random.randint(1, sides) for dice in range(num)]
@@ -43,11 +40,15 @@ def continue_check():
         else:
             print("please enter only yes or no")
 
-still_playing = True
-while still_playing:
-    num_dice = choose_num_dice()
-    num_sides = choose_num_sides()
-    roll_dice(num_dice, num_sides)
-    still_playing = continue_check()
-print("goodbye")
+def main():
+    still_playing = True
+    while still_playing:
+        num_dice = choose_num_dice()
+        num_sides = choose_num_sides()
+        roll_dice(num_dice, num_sides)
+        still_playing = continue_check()
+    print("goodbye")
+
+if __name__ == "__main__":
+    main()
    
